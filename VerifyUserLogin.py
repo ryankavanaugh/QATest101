783bd9fc3fa17084868e4750f9985602d53205e0
RyanJKavanaugh <32276600+RyanJKavanaugh@users.noreply.github.com>
2017-09-26 11:34:04 -0700

    Update VerifyUserLogin.py

--- VerifyUserLogin.py
+++ VerifyUserLogin.py
@@ -10,8 +10,13 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common import action_chains, keys
 from selenium.webdriver.common.action_chains import ActionChains
 import time
-import unittest
+from pyvirtualdisplay import Display
 # -*- coding: utf-8 -*-
+
+
+
+display = Display(visible=0, size=(800, 800))
+display.start()
 
 
 class Verify_Login_And_Saving_Routes(unittest.TestCase):
